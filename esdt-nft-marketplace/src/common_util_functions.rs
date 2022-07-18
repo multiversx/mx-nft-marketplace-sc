ac3807ad31c26b16d591c62ea423412309280618
dorin.iancu <dorin.iancu@elrond.com>
2022-07-18 13:16:01 +0300

    pause module

--- esdt-nft-marketplace/src/common_util_functions.rs
+++ esdt-nft-marketplace/src/common_util_functions.rs
@@ -1,12 +1,16 @@
 elrond_wasm::imports!();
 
 #[elrond_wasm::module]
-pub trait CommonUtilFunctions {
+pub trait CommonUtilFunctions: elrond_wasm_modules::pause::PauseModule {
     fn get_nft_info(&self, nft_type: &TokenIdentifier, nft_nonce: u64) -> EsdtTokenData<Self::Api> {
         self.blockchain().get_esdt_token_data(
             &self.blockchain().get_sc_address(),
             nft_type,
             nft_nonce,
         )
+    }
+
+    fn require_not_paused(&self) {
+        require!(self.not_paused(), "Marketplace is paused");
     }
 }
